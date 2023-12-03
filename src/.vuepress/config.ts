df03ccef48b09a39b34e2aa76b20b10a5865a0e4
Aaron Lippold <lippold@gmail.com>
2023-12-02 19:10:34 -0500

    fixed the config.ts for the GHPages deploy path
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: process.env.GITHUB_DEPLOY === "true" ? "/saf-training/" : "/",
+  base: process.env.GITHUB_DEPLOY === "true" ? "/saf-training-current/" : "/",
   lang: "en-US",
   title: "MITRE SAF Training",
   description: "The MITRE Security Automation Framework Training for Security Guidance, Hardening, Validation & Visualization",
