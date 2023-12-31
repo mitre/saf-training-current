b427709fc840a49a16cc3ced2c72aea1e2b17dd3
Aaron Lippold <lippold@gmail.com>
2023-12-31 18:19:11 -0500

    updating packages, still working on the cypress workflow
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

---
+++ cypress/e2e/site-alive.cy.js
@@ -0,0 +1,6 @@
+describe('The Site Loaded and has the expected Title', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:8080')
+    cy.title().should('eq', 'MITRE SAF Training | MITRE SAF Training')
+  })
+})
