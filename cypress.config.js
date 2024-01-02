64171af4415038ff0c22dc36ffd060ff2eecfc30
Aaron Lippold <lippold@gmail.com>
2024-01-02 18:27:07 -0500

    added cypress testing
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,15 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
+    baseUrl: 'http://localhost:8080',
   },
 });
+
+// const { defineConfig } = require('cypress')
+
+// module.exports = defineConfig({
+//   e2e: {
+//     baseUrl: 'http://localhost:8080',
+//   },
+// })
--- cypress/e2e/site-alive.cy.js
+++ cypress/e2e/site-alive.cy.js
@@ -1,4 +1,5 @@
 describe('The Site Loaded and has the expected Title', () => {
+  
   it('passes', () => {
     cy.visit('http://localhost:8080')
     cy.title().should('eq', 'MITRE SAF Training | MITRE SAF Training')
